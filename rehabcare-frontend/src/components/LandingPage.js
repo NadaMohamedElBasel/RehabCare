7adf50b0221f8345a00979b0b561dfd6db33d479
NadaMohamedElBasel <110432081+NadaMohamedElBasel@users.noreply.github.com>
2025-11-27 22:11:08 +0200

    added src frontend

---
+++ rehabcare-frontend/src/components/LandingPage.js
@@ -0,0 +1,37 @@
+// src/components/LandingPage.js
+import React from 'react';
+//import Navbar from './Navbar';
+import './LandingPage.css';
+
+// function LandingPage() {
+//   return (
+//     <div className="landing-container">
+//       <Navbar />
+//       <div className="landing-content">
+//         <h2>Welcome to RehabCare Portal</h2>
+//         <p>Please choose an option to get started:</p>
+//         <div className="landing-options">
+//           <a href="/register" className="option-button">Register</a>
+//           <a href="/login" className="option-button">Login</a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <div className="landing-content">
+        <h2>Welcome to RehabCare Portal</h2>
+        <p>Please choose an option to get started:</p>
+        <div className="landing-options">
+          <a href="/register" className="option-button">Register</a>
+          <a href="/login" className="option-button">Login</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
